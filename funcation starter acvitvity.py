91d5fcad07c118008480186d77581e84be80d82e
makaiW <144815658+makaiW@users.noreply.github.com>
2023-09-26 18:04:53 +0000

    new answers

--- funcation starter acvitvity.py
+++ funcation starter acvitvity.py
@@ -1,5 +1,5 @@
 # 1. In your own words, describe what a function is
- a relation between a set of inputs having one output each.
+ a relation between a set of inputs having one output each.a function is a relationship between inputs where each input is related to exactly one output.
 
 # 2. What is are function parameters and arguments and describe
 # the difference between the 2.
@@ -12,14 +12,14 @@ print{ welcome to pythonclass'}
 
 # 4. Write a function that will do a calculation that takes 3 parameters.
 # Your function can do any of the arithmatic operators (add, subtract, multiply, divide)
-number_of_pencils:100
-number_of_pens:213
+{number_of_pencils}100
+{number_of_pens}:213
 print{numberA + numberB}
 print{100+213}
 # 5. Write a function that will output a message to a user, telling them
 # what class they have next after this one. this code should use a 
 # variable to pass a value into the parameter of a function. The variable should
 # be real, user data- not hard coded data.
-print greeting{makai}
+print {welcome{makai}
 print {welcome_to_coding_class}
-print{next_class_math}
+print{next class {math}
