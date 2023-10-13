bcf76955ea1cdd8bbffcffccb7ab05c07da5ce41
makaiW <144815658+makaiW@users.noreply.github.com>
2023-10-13 15:21:31 +0000

    work is done

---
+++ looplesson2.py
@@ -0,0 +1,29 @@
+# python while - allow us to repeat blocks
+# of code indefinitely,unless told otherwise 
+
+
+
+# how do we write a while loop
+
+while 'example'== from 
+"run this code"
+
+def examplefuncation{}
+# how the codes
+# instrucation 
+
+
+
+def print{}
+# when user enters values 
+#instruction 
+if 0 > 1:
+    elif 0==1
+    else 0>1
+    while "this"=='this'
+    
+    for 'this'in 'that':
+
+
+
+
