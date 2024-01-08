973a2fd24426efdcc3ae66b91ba27a07122e9338
makaiW <144815658+makaiW@users.noreply.github.com>
2024-01-08 15:27:20 +0000

    done

---
+++ last python unit/pythonReveiw.py
@@ -0,0 +1,4 @@
+#To me computer programming is bascally giving it instruction and telling it what to do in order for it to funcation
+String-a sequence is used to store text 
+integer-a type to store whole number 
+float-is a type to store postive and negative numbers 
