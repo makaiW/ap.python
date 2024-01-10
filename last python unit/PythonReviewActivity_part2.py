3c6e1323ae89d267d70135072b706616d6f12ebf
makaiW <144815658+makaiW@users.noreply.github.com>
2024-01-10 16:22:07 +0000

    done

---
+++ last python unit/PythonReviewActivity_part2.py
@@ -0,0 +1,20 @@
+#  Winter break Python Mad libs 
+# Create a short mad lib story about your winter break. 
+# Your mad lib should include 6 input fields for users to enter data.
+# Your mad lib program should prompt the user to enter 2 nouns, 2 verbs, and 2 adjectives. 
+# Your mad lib should be written in complete sentences. 
+# Once completed, sub
+def story ()
+# madlibs story
+name  = input("makai")
+adjective = input("very cold ")
+adjective2  = input("no snow")
+noun = input("fast break")
+noun2 =input("the break was fun")
+plural_noun = input("i got what i wanted ")
+plural_noun2 = input ("hangout with mom and sister")
+ing_verb = input ("alot of eating")
+ing_verb2 = input ("alot of sleeping")
+place = input ("my house")
+adjective3 = input("cooked for all")
+print(story())
